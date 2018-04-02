0b1620ebca35897de042c64f4298aacc2aa9f685
Ryan Askew <ryan.askew@hotmail.com>
2018-04-02 15:18:00 -0500

    Add files via upload

---
+++ Data Structures/Assignment 5-2/cnPtrQueue.cpp
@@ -0,0 +1,69 @@
+#include "cnPtrQueue.h"
+#include <cassert>
+using namespace std;
+
+namespace CS3358_FA16_A5P2
+{
+    // constructor
+    cnPtrQueue::cnPtrQueue(): numItems(0){}
+
+    // return whether queue is empty or not
+    bool cnPtrQueue::empty() const
+    {
+        // queue is empty when there is no item in the outStack
+        // or the inStack
+        return (outStack.empty() && inStack.empty());
+    }
+
+    // returns # of items in queue
+    cnPtrQueue::size_type cnPtrQueue::size() const
+    {
+        return numItems;
+    }
+
+    // returns element at front of queue
+    CNode* cnPtrQueue::front()
+    {
+        // the queue cannot be empty
+        assert(!inStack.empty() || !outStack.empty());
+        // move inStack to outStack in order to access the front
+        // element of the queue
+        if (outStack.empty())
+        {
+            while (!inStack.empty())
+            {
+                outStack.push(inStack.top());
+                inStack.pop();
+            }
+        }
+        // return the front element of the queue
+        return outStack.top();
+    }
+
+    // pushes item to inStack
+    void cnPtrQueue::push(CNode* cnPtr)
+    {
+        inStack.push(cnPtr);
+        numItems++;
+    }
+
+    // removes the front element from the queue
+    void cnPtrQueue::pop()
+    {
+        // the queue cannot be empty
+        assert(!inStack.empty() || !outStack.empty());
+        // move inStack to outStack in order to access the front
+        // element of the queue
+        if (outStack.empty())
+        {
+            while (!inStack.empty())
+            {
+                outStack.push(inStack.top());
+                inStack.pop();
+            }
+        }
+        // remove the front item from the queue
+        outStack.pop();
+        numItems--;
+    }
+}
---
+++ Data Structures/Assignment 5-2/nodes_LLoLL.cpp
@@ -0,0 +1,87 @@
+#include "nodes_LLoLL.h"
+#include "cnPtrQueue.h"
+#include <iostream>
+using namespace std;
+
+namespace CS3358_FA16_A5P2
+{
+   void Destroy_cList(CNode*& cListHead)
+   {
+      int count = 0;
+      CNode* cNodePtr = cListHead;
+      while (cListHead != 0)
+      {
+         cListHead = cListHead->link;
+         delete cNodePtr;
+         cNodePtr = cListHead;
+         ++count;
+      }
+      cout << "Dynamic memory for " << count << " CNodes freed"
+           << endl;
+   }
+
+   void Destroy_pList(PNode*& pListHead)
+   {
+      int count = 0;
+      PNode* pNodePtr = pListHead;
+      while (pListHead != 0)
+      {
+         pListHead = pListHead->link;
+         Destroy_cList(pNodePtr->data);
+         delete pNodePtr;
+         pNodePtr = pListHead;
+         ++count;
+      }
+      cout << "Dynamic memory for " << count << " PNodes freed"
+           << endl;
+   }
+
+   // do depth-first traversal and print data
+   void ShowAll_DF(PNode* pListHead, ostream& outs)
+   {
+      while (pListHead != 0)
+      {
+         CNode* cListHead = pListHead->data;
+         while (cListHead != 0)
+         {
+            outs << cListHead->data << "  ";
+            cListHead = cListHead->link;
+         }
+         pListHead = pListHead->link;
+      }
+   }
+
+   // do breadth-first (level) traversal and print data
+   void ShowAll_BF(PNode* pListHead, ostream& outs)
+   {
+       // create a queue
+       cnPtrQueue q;
+       // start the cursor at the head of the main list
+       PNode * cursor = pListHead;
+       // add each sublist to the queue
+       while (cursor != 0)
+        {
+           if (cursor->data != 0)
+               q.push(cursor->data);
+
+           cursor = cursor->link;
+        }
+
+        // traverse the queue
+        while (!q.empty())
+        {
+            // print the front item's value
+            outs << q.front()->data << "  ";
+
+            // if there is another item in the front item's list,
+            // add it to the back of the queue. Once each sublist
+            // is empty, no more items will be added to the queue
+            // from that particular list.
+            if (q.front()->link != 0)
+               q.push(q.front()->link);
+            // remove the printed item from the queue
+            q.pop();
+        }
+
+   }
+}
